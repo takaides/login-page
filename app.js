f13d29bff466dfba279f6b240d824a2d9b6a4d56
takaides <takaides+github@gmail.com>
2017-08-14 09:58:06 -0400

    swapped out express-sessions for express-session

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // declarations
 const express = require('express');
 const handlebars = require('express-handlebars');
-const sessions = require('express-sessions');
+const sessions = require('express-session');
 const validator = require('express-validator');
 const body-parser = require('body-parser');
 const app = express();
